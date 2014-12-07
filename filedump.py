4656237d0c45d2ad1aaa85b02b252d3374f9be38
Zach Jasensky <ziggymh3@gmail.com>
2014-12-07 13:15:58 -0500

    began creating structure for sftp file transfers instead of rsync

--- filedump.py
+++ filedump.py
@@ -6,3 +6,15 @@ import ftplib
 import os
 import sys
 
+#create instance of FTP class
+ftps = ftplib.FTP('1.4.19.175')
+
+#connect to the host on port 22 for a secure connection
+ftps.connect('1.4.19.175', 22)
+
+
+#  need to make sure i can connect to the ftp server running on the pi using the above commands
+#  and then get a list of files from images directory. afterwards, transfer the files to a local
+#  directory and do some file housekeeping on the remote directory to make sure space is maintained
+#  maybe do a detection of space available and limit the number of images taken at any given time
+#  to that number, or just roll with it depending on how the surveying goes.
