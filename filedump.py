7d7af750ff11e54ca2141c768520b79f8f45bad3
Zach Jasensky <ziggymh3@gmail.com>
2014-12-09 13:12:39 -0500

    implemented simple socket-based echo server for testing

--- filedump.py
+++ filedump.py
@@ -2,19 +2,59 @@
 # our transfer will simply connect to the FTP server and pull all the files
 # currently present in the image capture directory and store them locally
 
-import ftplib
 import os
-import sys
+import socket
+import subprocess
+import re
 
-#create instance of FTP class
-ftps = ftplib.FTP('1.4.19.175')
+#setup some variables
+currDir = os.getcwd()
+gsIP = '1.4.19.116'  # needs to be updated to actual GS IP, probably of the 192.168.1.x variety
+droneIP = '1.4.19.175'  # needs to be updated to actual drone IP
+regex = '([0-9./])'  # regular expression for parsing ping statistics from ping output
 
-#connect to the host on port 22 for a secure connection
-ftps.connect('1.4.19.175', 22)
+#set up client socket for remote connection
+PORT = 5007
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-#  need to make sure i can connect to the ftp server running on the pi using the above commands
-#  and then get a list of files from images directory. afterwards, transfer the files to a local
-#  directory and do some file housekeeping on the remote directory to make sure space is maintained
-#  maybe do a detection of space available and limit the number of images taken at any given time
-#  to that number, or just roll with it depending on how the surveying goes.
+#ping the bastard to see if it's available, and grab the command output
+print 'Pinging Drone1...'
+p = subprocess.Popen(['ping', '-c', '3', droneIP], stdout=subprocess.PIPE)
+stdOutput, stdError = p.communicate()
+rc = p.returncode
+
+if rc == 0:
+    print('Drone1 with IP: %s is active' % droneIP)
+
+    #grab average ping - on the second to last line every time out of 9 line output (7)
+    avgPingStr = stdOutput.splitlines()[7]
+
+    #perform regex to capture min/avg/max/std-dev ping statistics
+    reStr = re.findall(regex, avgPingStr)
+    reStr = ''.join(reStr)
+
+    #separate out each entry delimited by '/' character
+    reStr = reStr.split('/')
+
+    #grab the average ping result in ms
+    avgPingStr = reStr[4]
+    avgPing = float(avgPingStr)
+
+    if avgPing <= 250:
+        print 'The average ping to Drone1 is ' + avgPingStr + 'ms'
+
+        print 'Connecting to Drone Server...'
+
+        s.connect((droneIP, PORT))
+        s.send('Hello World')
+        data = s.recv(1024)
+        s.close()
+        print 'Received', repr(data)
+
+
+elif rc == 2:
+    print('Drone1 with IP: %s did not respond' % droneIP)
+
+else:
+    print('Drone1 with IP: %s returned an error' %droneIP)
--- images.py
+++ images.py
@@ -5,46 +5,65 @@
 import os
 import picamera
 import time
+import socket
 
 camera = picamera.PiCamera()
 cwd = os.getcwd()
+time = time.time()  # doesn't do anything, just to make things happy
 imgDir = cwd + '/images'
 runTime = 60  # arbitrary capture time, should capture images for a minute
 sleepTime = 0.25  # sleep time for individual frame captures
 
-#calculate number of frames needed to fill allotted time and round to nearest integer
-FRAMES = int(runTime * sleepTime)
+#set up server socket for connections
+HOST = ''
+PORT = 5007
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by: ', addr
 
-camera.resolution = (2592, 1944)
-
-#capture images to a specified output path with the timestamp as the filename
-def captureImage(outputPath):
-    if os.path.exists(outputPath):
-        os.chdir(outputPath)
-        timeStr = time.strftime("%Y%m%d-%H%M%S")
-        camera.capture(timeStr + '.jpg')
-        print "Image captured with filename: " + timeStr + ".jpg"
-
-    else:
-        print "Output path: " + outputPath + "doesn't exist."
-
-#check to see if there is an existing output directory
-print 'Checking if /images exists...'
-
-if os.path.exists(imgDir):
-    print "It exists!"
-
-else:
-    print "It doesn't exist, creating directory"
-
-    os.makedirs(imgDir)
-    
-    print "Directory created."
+while 1:
+    data = conn.recv(1024)
+    if not data: break
+    conn.send(data)
+conn.close()
 
 
-#captures images to imgDir
-for frame in range(FRAMES):
-    captureImage(imgDir)
-    time.sleep(sleepTime)
-
-print 'Done capturing images.'
+#
+# #calculate number of frames needed to fill allotted time and round to nearest integer
+# FRAMES = int(runTime * sleepTime)
+#
+# camera.resolution = (2592, 1944)
+#
+# #capture images to a specified output path with the timestamp as the filename
+# def captureImage(outputPath):
+#     if os.path.exists(outputPath):
+#         os.chdir(outputPath)
+#         timeStr = time.strftime("%Y%m%d-%H%M%S")
+#         camera.capture(timeStr + '.jpg')
+#         print "Image captured with filename: " + timeStr + ".jpg"
+#
+#     else:
+#         print "Output path: " + outputPath + "doesn't exist."
+#
+# #check to see if there is an existing output directory
+# print 'Checking if /images exists...'
+#
+# if os.path.exists(imgDir):
+#     print "It exists!"
+#
+# else:
+#     print "It doesn't exist, creating directory"
+#
+#     os.makedirs(imgDir)
+#
+#     print "Directory created."
+#
+#
+# #captures images to imgDir
+# for frame in range(FRAMES):
+#     captureImage(imgDir)
+#     time.sleep(sleepTime)
+#
+# print 'Done capturing images.'
