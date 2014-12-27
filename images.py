941dea47a7f743849b5bc9129064606a69c0b056
Zach Jasensky <ziggymh3@gmail.com>
2014-12-27 15:48:32 -0500

    debugging sockets

--- images.py
+++ images.py
@@ -26,6 +26,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(2)  # should only be one client at a time, but made 2 for debugging purposes
 
+print 'yur a rite foul git'
+
 
 while True:
     (conn, addr) = s.accept()
