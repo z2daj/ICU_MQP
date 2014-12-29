3d11b184529c24de48eb660cfab6b13a6328f702
Zach Jasensky <ziggymh3@gmail.com>
2014-12-29 12:27:55 -0500

    debugging sockets

--- images.py
+++ images.py
@@ -53,6 +53,14 @@ def update_progress(progress):
     sys.stdout.flush()
 
 
+def countFiles(dir):
+    return len([name for name in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))])
+
+
+fileCount = countFiles(imgDir)
+
+print fileCount
+
 while True:
 
     req = conn.recv(sockBuff)
