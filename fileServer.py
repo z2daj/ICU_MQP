9e8c492f3d737da0246137358fb370df643900f4
Zach Jasensky <ziggymh3@gmail.com>
2014-12-30 17:14:27 -0500

    debugging sockets, beginning to add sending of each file in directory
    changed some files around to maintain functionality

--- fileServer.py
+++ fileServer.py
@@ -6,18 +6,13 @@
 #
 
 import os
-import picamera
 import time
 import socket
 import sys
 
-camera = picamera.PiCamera()
 cwd = os.getcwd()
 imgDir = cwd + '/images'
 imgPath = imgDir + '/'
-runTime = 60  # arbitrary capture time, should capture images for a minute
-sleepTime = 0.25  # sleep time for individual frame captures
-sockBuff = 4096
 
 global sz
 size = 2100000
@@ -123,41 +118,3 @@ while True:
 
 s.shutdown(socket.SHUT_RDWR)
 s.close()
-
-#
-# #calculate number of frames needed to fill allotted time and round to nearest integer
-# FRAMES = int(runTime * sleepTime)
-#
-# camera.resolution = (2592, 1944)
-#
-# #capture images to a specified output path with the timestamp as the filename
-# def captureImage(outputPath):
-#     if os.path.exists(outputPath):
-#         os.chdir(outputPath)
-#         timeStr = time.strftime("%Y%m%d-%H%M%S")
-#         camera.capture(timeStr + '.jpg')
-#         print "Image captured with filename: " + timeStr + ".jpg"
-#
-#     else:
-#         print "Output path: " + outputPath + "doesn't exist."
-#
-# #check to see if there is an existing output directory
-# print 'Checking if /images exists...'
-#
-# if os.path.exists(imgDir):
-#     print "It exists!"
-#
-# else:
-#     print "It doesn't exist, creating directory"
-#
-#     os.makedirs(imgDir)
-#
-#     print "Directory created."
-#
-#
-# #captures images to imgDir
-# for frame in range(FRAMES):
-#     captureImage(imgDir)
-#     time.sleep(sleepTime)
-#
-# print 'Done capturing images.'
