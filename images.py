09815a51972779cdb6535fdf6f64d6d50d9cc409
Chris <cmkeane@wpi.edu>
2015-01-07 12:47:08 -0500

    Formatting and test

--- images.py
+++ images.py
@@ -1,6 +1,6 @@
-#This python script creates an images directory and will take pictures from
-#the PiCamera every 10sec and store them with timestamps in the images
-#directory
+# This python script creates an images directory and will take pictures from
+# the PiCamera every 10sec and store them with timestamps in the images
+# directory
 
 import os
 import picamera
@@ -12,12 +12,12 @@ imgDir = cwd + '/images'
 runTime = 60  # arbitrary capture time, should capture images for a minute
 sleepTime = 0.25  # sleep time for individual frame captures
 
-#calculate number of frames needed to fill allotted time and round to nearest integer
+# calculate number of frames needed to fill allotted time and round to nearest integer
 FRAMES = int(runTime * sleepTime)
 
 camera.resolution = (2592, 1944)
 
-#capture images to a specified output path with the timestamp as the filename
+# capture images to a specified output path with the timestamp as the filename
 def captureImage(outputPath):
     if os.path.exists(outputPath):
         os.chdir(outputPath)
@@ -28,7 +28,7 @@ def captureImage(outputPath):
     else:
         print "Output path: " + outputPath + "doesn't exist."
 
-#check to see if there is an existing output directory
+# check to see if there is an existing output directory
 print 'Checking if /images exists...'
 
 if os.path.exists(imgDir):
@@ -42,7 +42,7 @@ else:
     print "Directory created."
 
 
-#captures images to imgDir
+# captures images to imgDir
 for frame in range(FRAMES):
     captureImage(imgDir)
     time.sleep(sleepTime)
