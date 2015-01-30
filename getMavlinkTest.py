6f231d4d0bf790ee07e1bc91bf8571e46026f3ba
Zach Jasensky <ziggymh3@gmail.com>
2015-01-30 14:44:05 -0500

    can't use generated mavlink header file

--- getMavlinkTest.py
+++ getMavlinkTest.py
@@ -2,11 +2,25 @@
 
 import sys
 import os
+import socket
+import re
+import select
+import time
 
 # import the pymavlink library and set up for use on APM
 d = os.getcwd()
-d += '/mavlink/pymavlink'
+d += '/mavlink'
 sys.path.append(d)
 
 import pymavlink
 
+# prepare for UDP connections and such
+HOST = ''
+mavproxy_port = 12345
+
+# now create the damn mavlink server
+mavproxy_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+mavproxy_sock.setblocking(0)
+mavproxy_sock.bind((HOST, mavproxy_port))
+
+mav_obj = incl.MAVLink(mavproxy_sock)
