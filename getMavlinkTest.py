d01d1334dd15900aae7caca2d4ba1cb94be51229
Zach Jasensky <ziggymh3@gmail.com>
2015-02-01 15:26:49 -0500

    fixed import error in getMavlinkTest.py by directly importing offending file in mavlinkv10.py

--- getMavlinkTest.py
+++ getMavlinkTest.py
@@ -26,4 +26,23 @@ mavproxy_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 mavproxy_sock.setblocking(0)
 mavproxy_sock.bind((HOST, mavproxy_port))
 
-mav_obj = mavlinkv10.MAVLink_message(mavproxy_sock)
+mav = mavlinkv10.MAVLink(mavproxy_sock)
+
+# set the WP_RADIUS parameter on the MAV at the end of the link
+mav.param_set_send(7, 1, "WP_RADIUS", 101)
+
+# alternatively, produce a MAVLink_param_set object
+# this can be sent via your own transport if you like
+m = mav.param_set_encode(7, 1, "WP_RADIUS", 101)
+
+# get the encoded message as a buffer
+b = m.get_msgbuf()
+
+# decode an incoming message
+m2 = mav.decode(b)
+
+# show what fields it has
+print("Got a message with id %u and fields %s" % (m2.get_msgId(), m2.get_fieldnames()))
+
+# print out the fields
+print(m2)
