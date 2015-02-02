9e4d85887b122c43ab6db0ee7b8cb1501694c3b6
Zach Jasensky <ziggymh3@gmail.com>
2015-02-02 13:06:30 -0500

    added some code to test GPS data reception and requesting GPS data
    needs testing

--- getMavlinkTest.py
+++ getMavlinkTest.py
@@ -4,9 +4,6 @@
 import sys
 import os
 import socket
-import re
-import select
-import time
 
 # import the pymavlink library and set up for use on APM
 d = os.getcwd()
@@ -16,6 +13,10 @@ sys.path.append(d)
 import mavlinkv10
 
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../'))
+
+# nececssary variables for requesting data from APM
+tgt_system = 0
+tgt_component = 0
 
 # prepare for UDP connections and such
 HOST = '127.0.0.1'
@@ -32,14 +33,64 @@ mav = mavlinkv10.MAVLink(mavproxy_sock)
 
 # Call to receive data over UDP socket, 1024 is the buffer size
 (data_from_mavproxy, address_of_mavproxy) = mavproxy_sock.recvfrom(1024)
-while True:
+
+try:
+    decoded_message = mav.decode(data_from_mavproxy)
+except MAVError as e:
+    print e
+
+# print('Got a message with id: %u, fields: %s, component: %d, System ID: %d' %(decoded_message.get_msgId(), decoded_message.get_fieldnames(), decoded_message.get_srcComponent(), decoded_message.get_srcSystem()))
+
+tgt_system = decoded_message.get_srcSystem()
+tgt_component = decoded_message.get_srcComponent()
+
+print 'System ID: %d, Component: %d' % (tgt_system, tgt_component)
+
+print 'Moving into test loop...'
+
+loopStat = True
+
+while loopStat:
+
     try:
         decoded_message = mav.decode(data_from_mavproxy)
     except MAVError as e:
         print e
 
-    print('Got a message with id: %u, fields: %s, component: %d, System ID: %d' %(decoded_message.get_msgId(), decoded_message.get_fieldnames(), decoded_message.get_srcComponent(), decoded_message.get_srcSystem()))
+    # some test code to see how to get GPS data from APM
+    # see if it's broadcast and grab it if so
+    # first gets the raw GPS data
+    if decoded_message.get_msgId() == mavlinkv10.MAVLink_MSG_ID_GPS_RAW_INT:
+        print 'Received a GPS message'
+        print('Got a message with id: %u, fields: %s, component: %d, System ID: %d' % (decoded_message.get_msgId(), decoded_message.get_fieldnames(), decoded_message.get_srcComponent(), decoded_message.get_srcSystem()))
+        print 'Decoded Message: '
+        print decoded_message
+        loopStat = False
 
-    # Prints the entire decode message
-    print 'Decoded Message: '
+    # check for broadcast filtered GPS pose (fused GPS and accel), in GPS-frame format
+    if decoded_message.get_msgId() == mavlinkv10.MAVLink_MSG_ID_GLOBAL_POSITION_INT_COV:
+        print 'Received GPS position'
+        print('Got a message with id: %u, fields: %s, component: %d, System ID: %d' % (decoded_message.get_msgId(), decoded_message.get_fieldnames(), decoded_message.get_srcComponent(), decoded_message.get_srcSystem()))
+        print 'Decoded Message: '
+        print decoded_message
+        loopStat = False
+
+
+gpsReq = True
+
+while gpsReq:
+
+    # create an encoded DATA_STREAM_ENCODE message with stream ID 6 (MAV_DATA_STREAM_POSITION)
+    encoded_message = mav.data_stream_encode(6, 1, 1)
+    mavproxy_sock.sendto(encoded_message, address_of_mavproxy)
+
+    (data_from_mavproxy, address_of_mavproxy) = mavproxy_sock.recvfrom(1024)
+
+    try:
+        decoded_message = mav.decode(data_from_mavproxy)
+    except MAVError as e:
+        print e
+
+    print 'Received Data Stream: '
     print decoded_message
+    gpsReq = False
