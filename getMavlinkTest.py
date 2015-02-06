c3028ba84b9d1e0df4b049cefbbcec409211a10c
Zach Jasensky <ziggymh3@gmail.com>
2015-02-06 14:53:36 -0500

    testing getting data from gps stream

--- getMavlinkTest.py
+++ getMavlinkTest.py
@@ -67,12 +67,15 @@ while loopStat:
     # some test code to see how to get GPS data from APM
     # see if it's broadcast and grab it if so
     # first gets the raw GPS data
-    if decoded_message.get_msgId() == mavlinkv10.MAVLINK_MSG_ID_GPS_RAW_INT:
-        print 'Received a GPS message'
-        print('Got a message with id: %u, fields: %s, component: %d, System ID: %d' % (decoded_message.get_msgId(), decoded_message.get_fieldnames(), decoded_message.get_srcComponent(), decoded_message.get_srcSystem()))
-        print 'Decoded Message: '
-        print decoded_message
-        loopStat = False
+
+    # this works
+
+    # if decoded_message.get_msgId() == mavlinkv10.MAVLINK_MSG_ID_GPS_RAW_INT:
+    #     print 'Received a GPS message'
+    #     print('Got a message with id: %u, fields: %s, component: %d, System ID: %d' % (decoded_message.get_msgId(), decoded_message.get_fieldnames(), decoded_message.get_srcComponent(), decoded_message.get_srcSystem()))
+    #     print 'Decoded Message: '
+    #     print decoded_message
+    #     loopStat = False
 
     # check for broadcast filtered GPS pose (fused GPS and accel), in GPS-frame format
     if decoded_message.get_msgId() == mavlinkv10.MAVLINK_MSG_ID_GLOBAL_POSITION_INT_COV:
@@ -87,21 +90,3 @@ while loopStat:
 
 print 'Received These Message IDs: '
 print IDs
-gpsReq = True
-
-while gpsReq:
-
-    # create an encoded DATA_STREAM_ENCODE message with stream ID 6 (MAV_DATA_STREAM_POSITION)
-    encoded_message = mav.request_data_stream_encode(1, 1, 6, 1, 1)
-    mavproxy_sock.sendto(encoded_message, address_of_mavproxy)
-
-    (data_from_mavproxy, address_of_mavproxy) = mavproxy_sock.recvfrom(1024)
-
-    # try:
-    decoded_message = mav.decode(data_from_mavproxy)
-    # except MAVError as e:
-    #     print e
-
-    print 'Received Data Stream: '
-    print decoded_message
-    gpsReq = False
