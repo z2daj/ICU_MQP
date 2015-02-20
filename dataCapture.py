02aefbd726131dca852a0697935f74484a713e02
Zach Jasensky <ziggymh3@gmail.com>
2015-02-20 16:56:21 -0500

    added wait for getData() thread inside dataCapture.py

--- dataCapture.py
+++ dataCapture.py
@@ -41,38 +41,41 @@ class dataCapture(object):
 
     def getData(self):
 
-        att = True
-        gps = True
+        while True:
 
-        while att or gps:
+            att = True
+            gps = True
 
-            (data_from_mavproxy, address_of_mavproxy) = self.mavproxy_sock.recvfrom(1024)
+            while att or gps:
 
-            try:
-                decoded_message = self.mav.decode(data_from_mavproxy)
-            except mavlinkv10.MAVError:
-                pass
+                (data_from_mavproxy, address_of_mavproxy) = self.mavproxy_sock.recvfrom(1024)
 
-            if decoded_message:
+                try:
+                    decoded_message = self.mav.decode(data_from_mavproxy)
+                except mavlinkv10.MAVError:
+                    pass
 
-                if decoded_message.get_msgId() == mavlinkv10.MAVLINK_MSG_ID_GPS_RAW_INT and gps:
-                    # print 'GPS Message Received'
-                    gps_time = decoded_message.time_usec
-                    lat = decoded_message.lat
-                    lon = decoded_message.lon
-                    alt = decoded_message.alt
-                    gps = False
+                if decoded_message:
 
-                if decoded_message.get_msgId() == mavlinkv10.MAVLINK_MSG_ID_ATTITUDE and att:
-                    # print 'Attitude Message Received'
-                    pitch = decoded_message.pitch
-                    roll = decoded_message.roll
-                    yaw = decoded_message.yaw
-                    att = False
+                    if decoded_message.get_msgId() == mavlinkv10.MAVLINK_MSG_ID_GPS_RAW_INT and gps:
+                        # print 'GPS Message Received'
+                        gps_time = decoded_message.time_usec
+                        lat = decoded_message.lat
+                        lon = decoded_message.lon
+                        alt = decoded_message.alt
+                        gps = False
 
-        print 'Data Collected.'
-        sample = lat, lon, alt, pitch, roll, yaw, gps_time  # creates a pose sample and appends it to sample list
-        self.samples.append(sample)
+                    if decoded_message.get_msgId() == mavlinkv10.MAVLINK_MSG_ID_ATTITUDE and att:
+                        # print 'Attitude Message Received'
+                        pitch = decoded_message.pitch
+                        roll = decoded_message.roll
+                        yaw = decoded_message.yaw
+                        att = False
+
+            print 'Data Collected.'
+            sample = lat, lon, alt, pitch, roll, yaw, gps_time  # creates a pose sample and appends it to sample list
+            self.samples.append(sample)
+            time.sleep(0.1)
 
     def getNextSample(self):
         return self.samples.pop()  # return a sample
